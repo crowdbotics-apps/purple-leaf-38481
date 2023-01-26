85398c3889074fc4fd522bdff6551937127fb359
Crowdbotics <team@crowdbotics.com>
2023-01-26 10:58:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-leaf-38481",
+  url: "https://purple-leaf-38481.botics.co",
+  api: "https://purple-leaf-38481.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
